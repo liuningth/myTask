8da68f96c90440b7d51d102cd99321c02f20dfef
liuningth <liuning_th@163.com>
2022-02-23 15:29:53 +0800

    add user's Eloquent ORM

---
+++ app/Repositories/UserRepository.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Tenant\User;
+use Prettus\Repository\Contracts\RepositoryInterface;
+
+/**
+ * Interface UserRepository.
+ *
+ * @package namespace App\Repositories;
+ */
+interface UserRepository extends RepositoryInterface
+{
+    /**
+     * @param string $name
+     * @return User[]
+     */
+    public function getUserByName(string $name);
+}
---
+++ app/Repositories/UserRepositoryEloquent.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Repositories;
+
+use Prettus\Repository\Eloquent\BaseRepository;
+use Prettus\Repository\Criteria\RequestCriteria;
+use App\Repositories\UserRepository;
+use App\Entities\User;
+use App\Validators\UserValidator;
+
+/**
+ * Class UserRepositoryEloquent.
+ *
+ * @package namespace App\Repositories;
+ */
+class UserRepositoryEloquent extends BaseRepository implements UserRepository
+{
+    /**
+     * Specify Model class name
+     *
+     * @return string
+     */
+    public function model()
+    {
+        return User::class;
+    }
+
+    /**
+     * @param string $name
+     * @return \Illuminate\Support\Collection|\App\Models\Tenant\User[]
+     */
+    public function getUserByName(string $name)
+    {
+        return $this->findByField('name', $name);
+    }
+
+
+
+}
